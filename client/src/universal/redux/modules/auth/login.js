783b4d1190712d661e0fe0935ca6f031181549a9
Mariocoski <mariuszrajczakowski@gmail.com>
2017-10-04 21:33:10 +0100

    small changes with form

--- client/src/universal/redux/modules/auth/login.js
+++ client/src/universal/redux/modules/auth/login.js
@@ -3,6 +3,8 @@ import {API_URL, JWT_TOKEN} from '../../../constants';
 const SET_LOGIN_PENDING = 'SET_LOGIN_PENDING';
 const SET_LOGIN_SUCCESS = 'SET_LOGIN_SUCCESS';
 const SET_LOGIN_ERROR = 'SET_LOGIN_ERROR';
+import { SubmissionError } from 'redux-form';
+import {toastr} from 'react-redux-toastr'
 
 // function makeAuthenticatedRequest(url, opts) {
 //   opts.headers = opts.headers || {};
@@ -47,11 +49,17 @@ export function loginUser(history,{email, password}){
         })
         .then(response => (response.json()))
         .then(response => {
+            console.log("here",response.status);
             localStorage.setItem(JWT_TOKEN, response.token);
             dispatch(setLoginPending(false));
             dispatch(setLoginSuccess(true));
             history.push('/dashboard');
         }).catch(error => {
+            console.log("here",error);
+            toastr.error(error.message);
+            throw new SubmissionError({
+                _error: error.message,
+              });
             console.log(error);
         });
     }
