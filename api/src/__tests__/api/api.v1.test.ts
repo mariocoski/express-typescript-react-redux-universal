09c72bf3513544e79c0051014d63ca38470c73b0
Mariocoski <mariuszrajczakowski@gmail.com>
2017-10-18 17:59:18 +0100

    added

---
+++ api/src/__tests__/api/api.v1.test.ts
@@ -0,0 +1,33 @@
+require('dotenv').config();
+describe('API V1', () => {
+
+  const request = require('supertest');  
+  let app: any;
+
+  beforeEach(() => {
+    app = require('../../server');
+  });
+
+  afterEach(() => {
+    app.close();
+  });
+
+  it('should respond with 200 for health check', async () => {
+    expect.assertions(1);
+    const response = await request(app).get('/api/v1');
+    expect(response.statusCode).toBe(200);
+  });
+
+  it('should respond with 404', async () => {
+    expect.assertions(2);
+    const response = await request(app).get('/not-existing-router');
+    expect(response.statusCode).toBe(404);
+    expect(response.text).toMatch('Not found');
+  });
+
+
+
+
+
+
+});
