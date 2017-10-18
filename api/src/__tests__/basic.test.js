1d10ef51f230c671396a6be549c81cd3ff2768cd
Mariocoski <mariuszrajczakowski@gmail.com>
2017-10-18 17:02:05 +0100

    fix

---
+++ api/src/__tests__/basic.test.js
@@ -0,0 +1,5 @@
+it('should work with plain js', () => {
+
+  expect(1).toBeTruthy();
+  expect([]).toEqual([]);
+});
--- api/src/__tests__/helpers/index.ts
+++
@@ -1,5 +0,0 @@
-export async function expectError(response: any, error:any){
-  expect.assertions(2);
-  expect(response.statusCode).toBe(422);
-  expect(response.text).toMatch(error);
-}
