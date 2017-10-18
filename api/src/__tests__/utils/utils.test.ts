c9618de096ecd677c6303c02b9c663dd43688082
Mariocoski <mariuszrajczakowski@gmail.com>
2017-10-18 16:35:49 +0100

    fix

--- api/src/__tests__/utils/utils.test.ts
+++ api/src/__tests__/utils/utils.test.ts
@@ -1,5 +1,5 @@
 import {env, seedDb, generateHash, getRoleId, formatError, catchErrors,
-        comparePassword, generateToken, resolvePort, handleError, onError} from '../../utils';
+        comparePassword, generateToken, resolvePort, handleError} from '../../utils';
 import {UnauthorizedError, BadRequestError, ForbiddenError, 
         NotFoundError, BaseError} from '../../lib/errors';
 import {USER_ROLE, ADMIN_ROLE, SUPERADMIN_ROLE} from '../../constants/roles.js';
