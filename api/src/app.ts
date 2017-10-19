d8b80cc8d5b2b98269f6187559fdea37ac2f89e1
Mariocoski <mariuszrajczakowski@gmail.com>
2017-10-19 16:50:02 +0100

    f

--- api/src/app.ts
+++ api/src/app.ts
@@ -3,7 +3,7 @@ import * as bodyParser from 'body-parser';
 import * as logger from 'morgan';
 import * as passport from 'passport';
 import * as cors from 'cors';
-// import * as expressValidator from 'express-validator';
+import * as expressValidator from 'express-validator';
 import * as compression from 'compression';
 import { Request, Response, NextFunction } from 'express';
 import { NotFoundError } from './lib/errors';
@@ -14,7 +14,7 @@ const app: express.Application = express();
 app.use(bodyParser.json()); 
 app.use(compression());
 app.use(bodyParser.urlencoded({ extended: false })); 
-// app.use(expressValidator());
+app.use(expressValidator());
 app.use(logger('dev'));
 
 const corsMiddleware = cors({ origin: '*', preflightContinue: true });
